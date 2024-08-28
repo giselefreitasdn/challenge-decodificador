be92582377dd6ea99843ffb805438f4949c4b460
Gisele Freitas <freitasgisele003@gmail.com>
2024-08-27 21:50:43 -0300

    Update funcoes.js

--- funcoes.js
+++ funcoes.js
@@ -1,27 +1,23 @@
 function criptografar() {
-    const input = document.getElementById('inputText').value;
-
-    let resultado = input
-        .toLowerCase()
+    const inputText = document.getElementById('inputText').value;
+    const encryptedText = inputText
         .replace(/e/g, 'enter')
         .replace(/i/g, 'imes')
         .replace(/a/g, 'ai')
         .replace(/o/g, 'ober')
         .replace(/u/g, 'ufat');
-
-    document.getElementById('outputText').innerText = resultado;
+    document.getElementById('result').innerText = encryptedText;
+    document.getElementById('result').style.display = 'block';
 }
 
 function descriptografar() {
-    const input = document.getElementById('inputText').value;
-
-    let resultado = input
-        .toLowerCase()
+    const inputText = document.getElementById('inputText').value;
+    const decryptedText = inputText
         .replace(/enter/g, 'e')
         .replace(/imes/g, 'i')
         .replace(/ai/g, 'a')
         .replace(/ober/g, 'o')
         .replace(/ufat/g, 'u');
-
-    document.getElementById('outputText').innerText = resultado;
+    document.getElementById('result').innerText = decryptedText;
+    document.getElementById('result').style.display = 'block';
 }
